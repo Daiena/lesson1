0381d902205795e9ce778b3b8eb02083acf3a1da
Daiena <daiena@mail.ru>
2023-05-16 15:55:43 +0300

    commonJS Модули

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-console.log("мой первый коммит");
-console.log("мой второй коммит");
-console.log("мой третий коммит и синхра");
+// console.log("мой первый коммит");
+// console.log("мой второй коммит");
+// console.log("мой третий коммит и синхра");
+const getValue=require("./helpers/array.js");
+console.log(getValue());
