df323ccd81df639c07c8c6d6ad27a6621ed78846
Daiena <daiena@mail.ru>
2023-05-17 11:01:56 +0300

    экспорт и импорт

--- helpers/array.js
+++ helpers/array.js
@@ -1,5 +1,18 @@
-function getValue(){
-    return "Привет мир";
-}
+// export function getValue(){
+//     return "Привет мир";
+// }
 
-module.exports=getValue;
+// export function getValue2(){
+//     return "Hello world";
+// }
+
+// export default getValue;
+const helpers={
+    getValue(){
+        return "Привет мир";
+    },   
+    getValue2(){
+        return "Hello world";
+    }
+}
+export default helpers;
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // console.log("мой первый коммит");
 // console.log("мой второй коммит");
 // console.log("мой третий коммит и синхра");
-const getValue=require("./helpers/array.js");
+// import {getValue, getValue2}  from "./helpers/array.js";
+// console.log(getValue());
+// console.log(getValue2());
+import array from "./helpers/array.js";
+// console.log(array);
+// console.log(array.getValue());
+// console.log(array.getValue2());
+const {getValue, getValue2}=array;
+console.log(array);
 console.log(getValue());
+console.log(getValue2());
