1295357c439d6149614ac9acfa099cfc82a748c7
Daiena <daiena@mail.ru>
2023-05-17 12:48:15 +0300

    динамический импорт

--- helpers/array.js
+++ helpers/array.js
@@ -1,18 +1,20 @@
-// export function getValue(){
-//     return "Привет мир";
-// }
+export function getValue(){
+    return "Привет мир";
+}
 
 // export function getValue2(){
 //     return "Hello world";
 // }
 
 // export default getValue;
-const helpers={
-    getValue(){
-        return "Привет мир";
-    },   
-    getValue2(){
-        return "Hello world";
-    }
-}
-export default helpers;
+
+
+// const helpers={
+//     getValue(){
+//         return "Привет мир";
+//     },   
+//     getValue2(){
+//         return "Hello world";
+//     }
+// }
+// export default helpers;
--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+// async function init(){
+// const {getValue}=await import("./helpers/array.js");
+// console.log(getValue());
+// }
+// init();
+
+async function init(){
+    if(false){
+        const {getValue}=await import("./helpers/array.js");
+        console.log(getValue());
+        }
+    }
+    init();
 // console.log("мой первый коммит");
 // console.log("мой второй коммит");
 // console.log("мой третий коммит и синхра");
 // import {getValue, getValue2}  from "./helpers/array.js";
 // console.log(getValue());
 // console.log(getValue2());
-import array from "./helpers/array.js";
+
+// import array from "./helpers/array.js";
 // console.log(array);
 // console.log(array.getValue());
 // console.log(array.getValue2());
-const {getValue, getValue2}=array;
-console.log(array);
-console.log(getValue());
-console.log(getValue2());
+
+
+// const {getValue, getValue2}=array;
+// console.log(array);
+// console.log(getValue());
+// console.log(getValue2());
